e277568c7fb1a70aea2fea3d8fe15b7622af761c
univalchemist <guix7777@outlook.com>
2021-12-22 22:33:53 +0800

    change plot pivot

--- plot_pivot.py
+++ plot_pivot.py
@@ -12,12 +12,12 @@ logger = logbook()
 parser = argparse.ArgumentParser(description='Set your Symbol, TradeAmount, PivotStep, DeltaPivot, DeltaSL, DeltaTrigger, StopLoss, Testnet. Example: "main.py -s BTCUSDT"')
 parser.add_argument('-s', '--symbol', default="BTCUSDT", help='str, Pair for trading e.g. "-s BTCUSDT"')
 parser.add_argument('-ps', '--pivotstep', default=5, type=int, help='int, Left/Right candle count to calculate Pivot e.g. "-ps 5"')
-parser.add_argument('-st', '--starttime', required=True, type=int, help='long, timestamp milliseconds for start time"-sl 1635768000000" ')
+parser.add_argument('-st', '--starttime', type=int, help='long, timestamp milliseconds for start time"-sl 1635768000000" ')
 parser.add_argument('-i', '--interval', default=1, type=int, help='int, time interval as minute"-sl 1" ')
 parser.add_argument('-test', '--testnet',  action="store_true", help='Run script in testnet or live mode.')
 args = parser.parse_args()
 
-client = Client(API_KEY, API_SECRET)
+client = Client(TEST_API_KEY, TEST_API_SECRET, testnet=True) if args.testnet else Client(API_KEY, API_SECRET)
 
 def main():
   maxLimit = int(1440 / args.interval)
@@ -26,9 +26,4 @@ def main():
   PlotPivot(res, args.pivotstep).draw_plot()
 
 if __name__ == "__main__":
-  if args.starttime == None:
-    logger.error("Please Check Start Time e.g. -st 1635768000000")
-    logger.error("exit!")
-    sys.exit()
-  else:
-    main()
+  main()
